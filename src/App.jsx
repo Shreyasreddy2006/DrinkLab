fdcd8d997e5595895ab61a35a703140743eaae49
Shreyasreddy2006 <shreyas.24bcs10152@sst.scaler.com>
2025-04-22 18:40:28 +0530

    logo issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 export default function App(){
   return(
     <>
-    <h1 className="app-title"><img src="./logo.png" alt="logo" />Every recipe is a love letter in a glass.</h1>
+    <h1 className="app-title"><img src="../logo.png" alt="logo" />Every recipe is a love letter in a glass.</h1>
     <div className="app-container">
       <Search></Search>
       <DataRec></DataRec>
