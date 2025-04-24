a498f9b0825c9cce9837366da1f0639a27bb01bb
Shreyasreddy2006 <shreyas.24bcs10152@sst.scaler.com>
2025-04-24 21:53:20 +0530

    logo issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { DataRec } from "./components/datarec";
 import { Search } from "./components/search";
 import { ReData } from "./store/ReData";
+import logo from "./assets/logo.png"
 import "./App.css";
 
 export default function App(){
   return(
     <>
-    <h1 className="app-title"><img src="../logo.png" alt="logo" />Every recipe is a love letter in a glass.</h1>
+    <h1 className="app-title"><img src={logo} alt="logo" />Every recipe is a love letter in a glass.</h1>
     <div className="app-container">
       <Search></Search>
       <DataRec></DataRec>
